0bee5fa0bd614f725b3c615eaa5ae376f487a220
ramezzateff <ramezatef62@gmail.com>
2023-07-23 11:01:41 -0700

    aa

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,6 +9,9 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	putchar(ch);
+	{
+		putchar(ch);
+	}
+	printf("\n");
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: count from 0 to 10
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
