080948808b8831ca5fb61b0cb0c19a9c19804524
ramezzateff <ramezatef62@gmail.com>
2023-07-24 13:10:09 -0700

    aa

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ * _isalpha - shows 1 if the input is a
+ *  Letter another case, shows 0
+ *  @c: the character in ASCII coe
+ *  Return: always 0 (success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('\n');
+}
