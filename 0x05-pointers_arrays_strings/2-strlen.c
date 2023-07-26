3094ff0ef2b48cfb948a0d9a3d16af46a1b8db80
ramezzateff <ramezatef62@gmail.com>
2023-07-26 02:17:34 -0700

    aa

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen - returns the length of a string
- * @s; string parameter inputs
+ * @s: string parameter inputs
  * Return; length of the string
 */
 int _strlen(char *s)
