716304fae8169c6ffdc5d6578bb6cafdd59c287f
ramezzateff <ramezatef62@gmail.com>
2023-08-18 00:56:55 -0700

    aa

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,9 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(arg, int));
-	}
+
 	if (i != (n - 1) && separator != NULL)
-	{
 		printf("%s", separator);
 	}
 	printf("\n");
