d114585e42cb5c3d5f4a67dd4c5b35249fc1466d
ramezzateff <ramezatef62@gmail.com>
2023-08-24 02:37:28 -0700

    lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,9 +11,9 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	unsigned int len = 0;
 
-	new = malloc(sizeof(list_t));
 	while (str[len])
 		len++;
+	new = malloc(sizeof(list_t));
 	if(!new)
 		return (NULL);
 	new->str = strdup(str);
