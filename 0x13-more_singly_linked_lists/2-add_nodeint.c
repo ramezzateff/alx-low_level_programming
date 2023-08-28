f6eaad2afa43b1f8c205b3285a752ca81d36f3b3
ramezzateff <ramezatef62@gmail.com>
2023-08-28 10:57:17 -0700

    LINKED_LIST

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,12 +1,12 @@
-#include "lists.h"
-
+#incluide "lists.h"
 /**
- * addnodeint.c - adds a new node at the beginning of a listint_t list.
+ * add_nodeint - adds a new node at the beginning of a linked list
  * @head: pointer to the first node in the list
- * @n: data to insert in that new code
+ * @n: data to insert in that new node
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: pointer to the new node, or NULL if it fails
  */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,10 @@
 #include "lists.h"
-
 /**
- * add_noteine_end - adds a new node at the end of a listint_t list.
+ * add_nodeint_end - adds a node at the end of a linked list
  * @head: pointer to the first element in the list
  * @n: data to insert in the new element
  *
- * Return: the address of the new element, or NULL if it failed
+ * Return: pointer to the new node, or NULL if it fails
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
