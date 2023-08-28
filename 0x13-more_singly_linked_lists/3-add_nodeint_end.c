c00371454bfcaceb94cb2e4321474e70eba648c5
ramezzateff <ramezatef62@gmail.com>
2023-08-28 03:36:52 -0700

    LINKED_LIST

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,6 +9,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *current = *head;
 	listint_t *end_node = malloc(sizeof(listint_t));
 
 	if (!end_node)
@@ -17,11 +18,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	end_node->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = end_node;
+		return (end_node);
+	}
 	else
 	{
-		listint_t *current = *head;
-
 		while (current->next != NULL)
 		{
 			current = current->next;
