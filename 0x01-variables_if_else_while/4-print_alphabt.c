baf39a9c6969104e294a5f4b65b745f999d08cfe
ramezzateff <ramezatef62@gmail.com>
2023-11-24 07:57:46 +0200

    aa

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: 'print alphabets lowercase'
+ * Return:Always 0
+ */
+int main(void)
+{
+	char exp = 'e', exp2 = 'q';
+
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == exp || ch == exp2)
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
