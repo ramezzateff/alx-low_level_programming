0b14be09eef1b199e8cf1351e269e1a3ffad17bf
ramezzateff <ramezatef62@gmail.com>
2023-11-24 09:36:26 +0200

    aa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry
+ * Description: prints numbers of base 16
+ * Return:Always 0
+ */
+int main(void)
+{
+	int hex;
+	char ch;
+	
+	for (hex = 0; hex < 10; hex++)
+		putchar(hex + '0');	
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
