7fdede60c1a473f9a5370071695ececcb31d6ae8
ramezzateff <ramezatef62@gmail.com>
2023-11-24 09:47:36 +0200

    aa

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,14 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
